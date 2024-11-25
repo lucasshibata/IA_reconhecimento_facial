0506f4058dd6e9f0a61d2532163a0b0810552c81
Lucas Pimentel Shibata <lucaspish@gmail.com>
2024-11-25 16:40:45 -0300

    novas mudanças

--- reconhecimento_facial.py
+++ reconhecimento_facial.py
@@ -2,8 +2,15 @@ import cv2
 from deepface import DeepFace
 
 #ler imagem
-imagem = cv2.imread("feliz.jpeg")
+imagem_feliz = cv2.imread("feliz.jpg")
+imagem_triste = cv2.imread("triste.jpg")
+imagem_surpreso = cv2.imread("surpresa.jpg")
+
 #passar imagem para deepface
-resultado = DeepFace.analyze(imagem, actions=("age", "emotion"))
+resultado_feliz = DeepFace.analyze(imagem_feliz, actions=("age", "emotion",'gender', 'race'), enforce_detection=False)
+resultado_triste = DeepFace.analyze(imagem_triste, actions=("age", "emotion",'gender', 'race'), enforce_detection=False)
+resultado_surpreso = DeepFace.analyze(imagem_surpreso, actions=("age", "emotion",'gender', 'race'), enforce_detection=False)
 #ver resultados
-print(resultado)
+print(resultado_feliz)
+print("\n",resultado_triste)
+print("\n",resultado_surpreso)
